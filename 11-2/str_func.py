852f22fc173a0aa3253287bb3ded0c2267ffa94f
Victor Sadykov <svn1409@gmail.com>
2023-07-13 23:15:21 +0800

    update str_func.py ad func my_str_title(text: str)

--- 11-2/str_func.py
+++ 11-2/str_func.py
@@ -3,3 +3,13 @@ def my_str_upper(text: str):
     возвращает строку text с заглавными буквами 
     """
     return text.upper()
+
+
+def my_str_title(text: str):
+    """
+    возвращает строку text c заглавными буквам каждого слова в строке
+    """
+    return ' '.join(word.title() for word in text.strip().split())
+
+
+# print(my_str_title('возвращает строку text c заглавными буквам каждого слова в строке'))
